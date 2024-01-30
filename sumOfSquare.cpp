f3c603bf1248b9efb42679a2d2a1d2c62046fe49
sonseldeep <sonseldeep8890.shrestha@gmail.com>
2024-01-30 14:26:18 +0545

    divideConqure

--- sumOfSquare.cpp
+++ sumOfSquare.cpp
@@ -1,3 +1,5 @@
+// sum of square of n numbers
+
 #include <iostream>
 using namespace std;
 int main()
@@ -6,11 +8,9 @@ int main()
   cout << "Enter N: ";
   cin >> n;
   int sum = 0;
-  // sum of square of n numbers
   for (int i = 1; i <= n; i++)
   {
     sum += i * i;
-    //  ?
   }
   cout << "Sum of square of n numbers : " << sum << endl;
 
