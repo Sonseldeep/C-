b86f4054f4f76d336a4c86f065db49615f1d1a37
sonseldeep <sonseldeep8890.shrestha@gmail.com>
2024-03-01 18:37:11 +0545

    type conversion

---
+++ typecasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char ch = 'a';
+  char Ch = 'A';
+  int result = (int)ch;
+  int Result = (int)Ch;
+  cout << "ASCI value of a : " << result << endl;
+  cout << "ASCI value of A : " << Result << endl;
+
+  return 0;
+}
